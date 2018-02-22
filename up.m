912a53d799b7d0be0e8ca24b0b593ff65bfe8cf6
Md Shahrear Zaman <36595882+shahrear86@users.noreply.github.com>
2018-02-22 15:36:42 +0100

    Add files via upload

---
+++ up.m
@@ -0,0 +1,9 @@
+%created by: shahrear
+%student.eco86@gmail.com
+%January2018
+%UP
+%reference: https://www.gnu.org/software/octave/octave.pdf
+r = linspace (numel (0:0.1:10*pi), 1, 1 \ numel (0:0.1:10*pi));
+plot3 (r.*sin (0:0.1:10*pi), r.*cos (0:0.1:10*pi),0:0.1:10*pi)
+%************************************************
+

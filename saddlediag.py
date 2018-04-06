b17e7e410f764a2f46d670bf64e19174f8d7382a
Md Shahrear Zaman <36595882+shahrear86@users.noreply.github.com>
2018-04-06 16:18:43 +0200

    Add files via upload

---
+++ saddlediag.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 13:49:49 2018
+
+@author: shahrear
+reference book:
+1.Economic Dynamics
+Phase Diagrams and Their Economic Application
+Second Edition
+2002
+by 
+RONALD SHONE
+University of Stirling
+
+Chapter-4: Systems of first-order differential equations
+Page-171
+Example 4.13
+
+2.User's Guide
+NumPy User Guide
+Release 1.11.0
+Written by the NumPy community
+May 29, 2016
+
+3.User's Guide
+Matplotlib
+Release 2.1.0
+by John Hunter, Darren Dale, Eric Firing, Michael Droettboom and the m
+October 07, 2017
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+X, Y = np.meshgrid(np.arange(-10, 10, 1), np.arange(-10, 10, 1))
+Xdot=X+Y
+Ydot=4*X+Y
+plt.figure()
+plt.title('Phase Diagram:Saddle Path')
+Q=plt.quiver(X,Y,Xdot,Ydot,units='width')
+plt.show()
+
+
+
+

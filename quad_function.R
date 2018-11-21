7c56f373b0fefd89b6828fb27e7fb0779ad89517
Md Shahrear Zaman <36595882+shahrear86@users.noreply.github.com>
2018-11-21 17:55:50 +0100

    Add files via upload

---
+++ quad_function.R
@@ -0,0 +1,74 @@
+ #created by : shahrear
+ #student.eco86@gmail.com
+ 
+ 
+ myfunction <- function(x,y,p) 
+ {
+  (x+y)^p
+ }
+x <- seq(0,100) 
+y <- seq(0,100)
+p= 2
+z = outer(x,y,myfunction)
+boxplot(z)
+hist(z)
+persp(x,y,z)
+contour(x,y,z)
+
+# by considering some random z named check_new_z 
+check_new_z = rnorm(101) 
+boxplot(check_new_z)
+hist(check_new_z)
+plot(x,check_new_z)
+
+
+
+myfunction1 <- function(x,y,p) 
+{
+ z1 = (x+y)^p
+ return(z1)
+}
+z1=myfunction1(x,y,2)
+boxplot(z1) 
+plot(x,z1)
+hist(z1)
+
+
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
